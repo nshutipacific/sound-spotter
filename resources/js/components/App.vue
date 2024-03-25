bb31740621c3430e03fda8e0bfe0933bb090aefd
Pacifique Nshuti <pacific.nsuti@zatechub.com>
2024-03-25 09:29:22 +0200

    Initial commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1 class="bg-gray-500 p-6">
+       Sound Spotter Setup 
+    </h1>
+</template>
+<script>
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sound Spotter Home</title>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+</head>
+<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <div id="app"></div>
+</body>
+</html>
