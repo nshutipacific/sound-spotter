6f99de23ff974c43cd2e8c5de08940db14445aae
Pacifique Nshuti <pacific.nsuti@zatechub.com>
2024-03-27 14:06:26 +0200

    added navigation component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,12 @@ import './bootstrap';
 import { createApp } from "vue";
 import App from "./components/App.vue";
 import Dashboard from './components/dashboard/Dashboard.vue';
+import Navigation from './components/Navigation.vue';
 
 const app = createApp({});
 app.component('app', App);
 app.component('dashboard', Dashboard);
+app.component('navigation', Navigation);
 app.mount("#app");
 
 
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -6,28 +6,7 @@
             <div class="p-1 text-2xl">
                 SoundSpotter
             </div>
-            <!--Navigation-->
-            <div class="mt-8">
-                <div class="mt-8">
-                    <router-link to="/dashboard"
-                        class="p-2 block text-white rounded-md hover:bg-gray-900 cursor-pointer">Home</router-link>
-                    <router-link to="/profile"
-                        class="p-2 block text-white rounded-md hover:bg-gray-900 cursor-pointer">Genres</router-link>
-                    <router-link to="/settings"
-                        class="p-2 block text-white rounded-md hover:bg-gray-900 cursor-pointer">Artists</router-link>
-                    <router-link to="/settings"
-                        class="p-2 block text-white rounded-md hover:bg-gray-900 cursor-pointer">Albums</router-link>
-                </div>
-                <div class="mt-8 mb-8">
-                    <!--Link for favourites Album and favourite artists-->
-                    <router-link to="/favourites"
-                        class="p-2 block text-white rounded-md hover:bg-gray-900 cursor-pointer">Favourites
-                        Artists</router-link>
-                    <router-link to="/favourites"
-                        class="p-2 block text-white rounded-md hover:bg-gray-900 cursor-pointer">Favourites
-                        Albums</router-link>
-                </div>
-            </div>
+            <navigation />
 
         </div>
         <!--Contents-->
