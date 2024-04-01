3188f29851240ed496aec0473499d697fb391a8f
Pacifique Nshuti <pacific.nsuti@zatechub.com>
2024-04-01 16:17:50 +0200

    revising the google login button

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -4,14 +4,51 @@
         <!--Search bar and login button-->
         <div class="flex justify-between p-6">
             <div>
-                <input type="search" class="rounded-lg bg-gray-600 text-xs px-4 py-2 outline-none text-white"
-                    placeholder="Search artists, albums">
+                <div class="text-gray-200 font-bold">Welcome to SoundSpotter</div>
+                <div class="text-xs">
+                    <p>
+                        Sound spotter is implemented using Laravel and Vue.js
+                    </p>
+                    <p>
+                        It uses the Last.fm API to fetch the latest albums
+                    </p>
+                    <p>
+                        Click on the Google button to login
+                    </p>
+                    <p>
+                        Once logged in, you can search for albums and save them to your account
+                    </p>
+                </div>
             </div>
             <div>
                 <a href="auth/google">
-                    <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png"
-                        alt="">
+                    <div class="gsi-material-button">
+                        <div class="gsi-material-button-state"></div>
+                        <div class="gsi-material-button-content-wrapper">
+                            <div class="gsi-material-button-icon">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"
+                                    xmlns:xlink="http://www.w3.org/1999/xlink" style="display: block;">
+                                    <path fill="#EA4335"
+                                        d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z">
+                                    </path>
+                                    <path fill="#4285F4"
+                                        d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z">
+                                    </path>
+                                    <path fill="#FBBC05"
+                                        d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z">
+                                    </path>
+                                    <path fill="#34A853"
+                                        d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z">
+                                    </path>
+                                    <path fill="none" d="M0 0h48v48H0z"></path>
+                                </svg>
+                            </div>
+                            <span class="gsi-material-button-contents">Sign in</span>
+                            <span style="display: none;">Sign in with Google</span>
+                        </div>
+                    </div>
                 </a>
+
             </div>
         </div>
         <!-- Featured albums-->
@@ -36,8 +73,8 @@
                         <img class="h-32 rounded-md" :src="album.image[2]['#text']" :alt="album.name">
                     </div>
                     <div class="mt-2 text-gray-300 font-bold whitespace-nowrap overflow-hidden overflow-ellipsis">
-                         {{ album.name }}</div>
-                    <div>{{ album.artist}}</div>
+                        {{ album.name }}</div>
+                    <div>{{ album.artist }}</div>
                 </div>
             </div>
         </div>
